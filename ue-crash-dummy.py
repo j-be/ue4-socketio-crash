f953eac16383e4ac7ee4c3678340665a3af41943
Juri Berlanda <juriberlanda@hotmail.com>
2017-05-27 17:06:32 +0200

    Add rudimentary tests with and without Exception.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

---
+++ ue-crash-dummy.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_socketio import SocketIO
+
+
+## Instanciate Flask (Static files and REST API)
+app = Flask(__name__)
+## Instanciate SocketIO (Websockets, used for events) on top of it
+socketio = SocketIO(app)
+
+
+@socketio.on('testEvent')
+def test_event():
+    print "Got test event"
+
+@socketio.on('testEventWithException')
+def test_event_with_exception():
+    print "Got test event, throwing exception NOW"
+    raise Exception()
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0')
